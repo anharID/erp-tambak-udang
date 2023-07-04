848dedee605bcd7c90e190f758d0342c966a1659
anharID <63109799+anharID@users.noreply.github.com>
2023-07-04 14:55:47 +0700

    fix

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -62,7 +62,7 @@
         <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg" aria-hidden="true"></span>
         @endif
         <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-          href="">
+          href="{{ route('inventaris.index') }}">
           <i class="fa-solid fa-warehouse fa-sm"></i>
           <span class="ml-4">Manajemen Inventaris</span>
         </a>
